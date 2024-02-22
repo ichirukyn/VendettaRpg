4d30259cf0a8ca7b42c7a3f9b78a06b61534163d
ichirukyn <ichiru.kyn@bk.ru>
2024-02-22 21:41:46 +0300

    fix: add URL to Env

--- tgbot/api/__init__.py
+++ tgbot/api/__init__.py
@@ -1,4 +1,15 @@
-URL = 'http://localhost:3000'
+import pathlib
+import sys
+
+from environs import Env
+
+script_path = pathlib.Path(sys.argv[0]).parent.parent.parent
+path = f"{script_path}\\.env"
+
+env = Env()
+env.read_env(path)
+
+URL = env.str('API_URL')
 HEADERS = {}
 
 
