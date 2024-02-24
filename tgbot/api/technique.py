bfffe7ec24c3a38fad96ce8535e1f3cf520b3824
ichirukyn <ichiru.kyn@bk.ru>
2024-02-24 22:27:35 +0300

    fix: Отображение техник

--- tgbot/api/technique.py
+++ tgbot/api/technique.py
@@ -16,6 +16,6 @@ async def get_technique(session, technique_id: int) -> TechniqueType:
         return await res.json()
 
 
-async def fetch_technique(session, class_id, race_id) -> [TechniqueType]:
+async def fetch_technique(session, race_id, class_id) -> [TechniqueType]:
     async with session.get(url(f'/technique'), params={'class_id': class_id, 'race_id': race_id}) as res:
         return await res.json()
